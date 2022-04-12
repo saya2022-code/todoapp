c6fc0b0e18e61c433f2248a045e161572772f95a
saya2022-code <s.nishizaka@g.dreamcareer.co.jp>
2022-04-12 19:41:28 +0900

    投稿APIのルーティング

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,18 +1,29 @@
-var express = require("express");
-const items = require("../../src/items");
+// var express = require("express");
+// const items = require("../../src/items");
 
+// var router = express.Router();
+
+// /* 商品一覧を取得するルーティング */
+// router.get("/items", function (req, res, next) {
+//   const itemsList = items.getListItem();
+//   res.send(itemsList);
+// });
+
+// /*１件の商品情報を取得するルーティング */
+// router.get("/items/:id", function (req, res, next) {
+//   const item = items.getItem(req.params.id);
+//   res.send(item);
+// });
+var express = require("express");
 var router = express.Router();
 
-/* 商品一覧を取得するルーティング */
-router.get("/items", function (req, res, next) {
-  const itemsList = items.getListItem();
-  res.send(itemsList);
-});
+const tasks = require("../../src/tasks/create.js");
 
-/*１件の商品情報を取得するルーティング */
-router.get("/items/:id", function (req, res, next) {
-  const item = items.getItem(req.params.id);
-  res.send(item);
-});
+/*タスクを登録するルーティング */
+router.post("/tasks", function(req, res, next){
+  console.log(req,body);
+  const createTask = create.postCreateTasks(req,body);
+  res.send(createTask);
+  });
 
 module.exports = router;
--- src/tasks/create.js
+++ src/tasks/create.js
@@ -1,1 +1,20 @@
 // 新規登録処理
+const mysql = require("mysql2/promise"); //mariaDBを指定
+const { dbSetting } = require("../../config.js");
+const config = require("../config.js");
+
+/*タスクを登録するルーティング */
+postCreateTasks = async function(){
+  let connection = null;
+  try{
+    connection = await mysql.createConnection(config,dbSetting);
+    //ここにsql
+    return "123";
+  }catch(e) {
+    console.log(e);
+  }finally{
+    connection.end();
+  }
+};
+
+exports.postCreateTasks = postCreateTasks;
