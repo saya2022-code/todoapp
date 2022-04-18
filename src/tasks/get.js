e4ffd8eb4461f73f2fe5e679161e9a67bd3a3a53
saya2022-code <s.nishizaka@g.dreamcareer.co.jp>
2022-04-18 17:01:09 +0900

    1件取得API-画面表示(途中)

--- src/tasks/get.js
+++ src/tasks/get.js
@@ -1,20 +1,19 @@
-// 1件の情報取得処理
-/*タスクを登録するルーティング */
-getTaskId = async function(id){
-  let connection = null;
-  try{
-    connection = await mysql.createConnection(config.dbSetting);
-    //ここにsql
-    const sql = "SELECT t_task.id, t_task.category_id, m_category.category_name, t_task.task_name, t_task.deadline, t_task.task_status, t_task.updated_at, t_task.created_at FROM t_task LEFT JOIN m_category ON t_task.category_id = m_category.id WHERE t_task.id = ?";
-    let d = [id];
-    const[rows,fields] = await connection.query(sql,d);
+// // 1件の情報取得処理
+// getTaskId = async function(id){
+//   let connection = null;
+//   try{
+//     connection = await mysql.createConnection(config.dbSetting);
+//     //ここにsql
+//     const sql = "SELECT t_task.id, t_task.category_id, m_category.category_name, t_task.task_name, t_task.deadline, t_task.task_status, t_task.updated_at, t_task.created_at FROM t_task LEFT JOIN m_category ON t_task.category_id = m_category.id WHERE t_task.id = ?";
+//     let d = [id];
+//     const[rows,fields] = await connection.query(sql,d);
     
-    return rows;
-  }catch(e) {
-    console.log(e);
-  }finally{
-    connection.end();
-  }
-};
+//     return rows;
+//   }catch(e) {
+//     console.log(e);
+//   }finally{
+//     connection.end();
+//   }
+// };
 
-exports.getTaskId = getTaskId;
+// exports.getTaskId = getTaskId;
--- src/tasks/list.js
+++ src/tasks/list.js
@@ -9,7 +9,7 @@ getListTask = async function(body){
     connection = await mysql.createConnection(config.dbSetting);
     //ここにsql
 // return "abc";
-    const sql ="SELECT t_task.task_name,t_task.deadline,m_category.id,m_category.category_name,t_task.task_status FROM t_task INNER JOIN m_category ON t_task.category_id = m_category.id";
+    const sql ="SELECT t_task.id,t_task.task_name,t_task.deadline,t_task.category_id,m_category.category_name,t_task.task_status FROM t_task INNER JOIN m_category ON t_task.category_id = m_category.id";
     let d = [];
     const[rows,fields] = await connection.query(sql,d);
     return rows;
@@ -20,4 +20,23 @@ getListTask = async function(body){
   }
 };
 
+// 1件の情報取得処理
+getTaskId = async function(id){
+  let connection = null;
+  try{
+    connection = await mysql.createConnection(config.dbSetting);
+    //ここにsql
+    const sql = "SELECT t_task.id, t_task.category_id, m_category.category_name, t_task.task_name, t_task.deadline, t_task.task_status, t_task.updated_at, t_task.created_at FROM t_task LEFT JOIN m_category ON t_task.category_id = m_category.id WHERE t_task.id = ?";
+    let d = [id];
+    const[rows,fields] = await connection.query(sql,d);
+    
+    return rows;
+  }catch(e) {
+    console.log(e);
+  }finally{
+    connection.end();
+  }
+};
+
 exports.getListTask = getListTask;
+exports.getTaskId = getTaskId;
