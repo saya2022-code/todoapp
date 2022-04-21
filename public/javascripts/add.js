1de97d83292389bc2b4dce09dc457f0374725055
saya2022-code <s.nishizaka@g.dreamcareer.co.jp>
2022-04-21 14:08:09 +0900

    モーダルのCSSを整える

--- public/javascripts/add.js
+++ public/javascripts/add.js
@@ -1,3 +1,13 @@
+$("#taskName").focusout(function () {
+  const current = $(this).val().lengh;
+    if(current < 1){
+        $("#taskName-error").text("文字を入力して下さい").css("color","red");
+    }else if(current > 0){
+        $("#taskName-error").text("");
+    }
+  });
+  
+
 // $("#addtaskName").focus(function () {
 //     const current = $(this).val().length;
 //       if(current < 1){
@@ -7,11 +17,3 @@
 //       }
 //     });
 
-// // $("#taskName").focus(function () {
-// //     const curren = $(this).val().lengh;
-// //       if(curren < 1){
-// //           $("#Name-error",).text("文字を入力して下さい").css("color","red");
-// //       }else if(curren > 0){
-// //           $("#Name-error").text("");
-// //       }
-// //     });
