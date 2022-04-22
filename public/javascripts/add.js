e533d9a1e10b3f45c9cc8f13e9cdc45ff685e55f
saya2022-code <s.nishizaka@g.dreamcareer.co.jp>
2022-04-22 15:30:02 +0900

    タスク数カウント(途中2)

--- public/javascripts/add.js
+++ public/javascripts/add.js
@@ -1,34 +1,35 @@
-$("#taskName").focusout(function () {
-  const current = $(this).val().length;
-    if(current < 1){
-        $("#taskName-error").text("文字を入力して下さい").css("color","red");
-    }else if(current > 0){
-        $("#taskName-error").text("");
-    }
-  });
+// $("#taskName").focusout(function () {
+//   const current = $(this).val().length;
+//     if(current < 1){
+//         $("#taskName-error").text("文字を入力して下さい").css("color","red");
+//     }else if(current > 0){
+//         $("#taskName-error").text("");
+//     }
+//   });
   
 
 
 //ステータスで色を変える
-$(async function() {
+const listIn = $(async function() {
   //データの取得
   const dataIn = await httpGet("//" + window.location.host +"/api/list");
 
-  const listIn = dataIn.map((item) => {
+  dataIn.map((item) => {
   console.log(item);
-
   
-//   const statusColor = item.task_status; 
-//   console.log(statusColor);
-  
-//   var statusColorName = document.getElementById("td-head");
-  
-// if(statusColor == 1){
-//   statusColorName.style.backgroundColor ='red';
-   
-//   }else if(statusColor == 2){
-//     statusColorName.style.backgroundColor ='blue';
-//   };
- });
+  const statusColor = item.task_status; 
+  console.log(statusColor);
+  })
 });
+  var statusColorName = document.getElementById("td-head");
+  
+//   if(statusColor == 1){
+//     document.getElementById("td-head").style.backgroundColor ='red'
+   
+//   } else if(statusColor == 2){
+//     document.getElementById("td-head").style.backgroundColor ='blue'
+//   };
+//  });
+//  $(".tdh").append(listIn);
+// });
 
