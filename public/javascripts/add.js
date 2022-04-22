2d90c7337cefd8e5c9702357b1995cfaf0c8e926
saya2022-code <s.nishizaka@g.dreamcareer.co.jp>
2022-04-22 11:27:53 +0900

    ステータスの色分け(途中)

--- public/javascripts/add.js
+++ public/javascripts/add.js
@@ -9,4 +9,26 @@ $("#taskName").focusout(function () {
   
 
 
+//ステータスで色を変える
+$(async function() {
+  //データの取得
+  const dataIn = await httpGet("//" + window.location.host +"/api/list");
+
+  const listIn = dataIn.map((item) => {
+  console.log(item);
+
+  
+//   const statusColor = item.task_status; 
+//   console.log(statusColor);
+  
+//   var statusColorName = document.getElementById("td-head");
+  
+// if(statusColor == 1){
+//   statusColorName.style.backgroundColor ='red';
+   
+//   }else if(statusColor == 2){
+//     statusColorName.style.backgroundColor ='blue';
+//   };
+ });
+});
 
